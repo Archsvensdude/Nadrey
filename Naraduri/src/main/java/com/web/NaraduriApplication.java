0e77f79399d543caef80425655dbf3efebe9239a
Archsvensdude <junepsgv1357@naver.com>
2024-09-30 18:23:07 +0900

    Add files via upload

---
+++ Naraduri/src/main/java/com/web/NaraduriApplication.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaraduriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaraduriApplication.class, args);
+	}
+
+}
